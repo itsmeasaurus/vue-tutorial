051832bbc8304b4f309d91195b7135661416bf10
LIAMPC\htetw <htetwaiyanaung.acc@gmail.com>
2023-04-16 23:35:08 +0630

    Section 3 : Handle a form submission

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -5,6 +5,13 @@ export default {
         <section class="space-y-6">
             <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+
+            <form @submit.prevent="add">
+                <div class="text-black border border-gray-600">
+                    <input v-model='newAssignment' placeholder="New Assignment ..." class="p-2"/>
+                    <button type="submit" class="bg-white p-2 border-l">Add</button>
+                </div>
+            </form>
         </section>
     `,
     data() {
@@ -14,6 +21,7 @@ export default {
                 { id:2, name: 'Read Chapter 4', complete: false},
                 { id:3, name: 'Turn in homework', complete: false},
             ],
+            newAssignment: ''
         }
     },
     computed: {
@@ -22,6 +30,16 @@ export default {
                 inProgress: this.assignments.filter(assignment => !assignment.complete),
                 completed: this.assignments.filter(assignment => assignment.complete)
             }
+        }
+    },
+    methods: {
+        add() {
+            this.assignments.push({
+                id: this.assignments.length + 1,
+                name: this.newAssignment,
+                complete: false
+            })
+            this.newAssignment = ''
         }
     }
 }
