928d14794916f5a3bb6578f98128a89892532a96
LIAMPC\htetw <htetwaiyanaung.acc@gmail.com>
2023-04-17 00:03:18 +0630

    Section 4 : It's all easy

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -3,11 +3,27 @@ export default {
     components: {Assignment},
     template: `
         <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">{{title}} ({{ assignments.length}})</h2>
+            <h2 class="font-bold mb-2">
+                {{title}}
+                <span>{{ assignments.length}}</span>
+            </h2>
 
-            <ul class="border border-gray-600 divide-y divide-gray-600">
+            <div class="flex gap-2 align-center">
+                <button
+                    @click="currentTag = tag"
+                    v-for="tag in tags"
+                    class="border rounded px-1 py-px text-xs"
+                    :class="{
+                        'border-blue-500 text-blue-500' : tag === currentTag
+                    }"
+                >
+                {{tag}}
+                </button>
+            </div>
+
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                <assignment
-                    v-for="assignment in assignments"
+                    v-for="assignment in filteredAssignments"
                     :key="assignment.id"
                     :assignment="assignment"
                 >
@@ -18,5 +34,21 @@ export default {
     props: {
         assignments: Array,
         title: String
+    },
+    data() {
+        return {
+            'currentTag': 'all'
+        }
+    },
+    computed: {
+        filteredAssignments() {
+            if(this.currentTag === 'all'){
+                return this.assignments;
+            }
+            return this.assignments.filter(assignment => assignment.tag === this.currentTag)
+        },
+        tags() {
+            return ['all', ...new Set(this.assignments.map(assignment => assignment.tag))];
+        }
     }
 }
